0658649e4c09d9e0815fda11826e28e4e51e9eab
taku <moldigai@yahoo.co.jp>
2023-05-04 17:24:44 +0900

    [Add] index

---
+++ atcoder295c1.java
@@ -0,0 +1,22 @@
+//自分で解いてみる、解けなかった
+import java.util.*;
+import java.util.Arrays;
+
+public class atcoder295c1 {
+    public static void main(String[] args ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] color = new int[N];  //N行の整数配列を定義
+        for(int i=0;i<N;i++){
+            color[i]=sc.nextInt();
+        }  //ここまでで読み込み完了
+
+        // for(int i=0;i<N;i++){
+        //     System.out.println(color[i]);  //最低限forループで出力
+        // }
+        // System.out.println(Arrays.toString(color));  //arrayをimportして配列で出力
+
+
+        //得られた配列の中身をカウントする方法がわからなかった
+    }
+}
